f6745eae6ad522522d4f15f80b4024e2b905d067
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-09-25 00:19:43 -0500

    all added but image fix, readme, and fix for particle system

--- src/Pages/MainLayout/index.jsx
+++ src/Pages/MainLayout/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router";
-
+import particlesJS from "particles.js";
 
 export default function MainLayout() {
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    particlesJS.load('particles-js', '../../assets/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
     return (
-        <div>
+        <div id="particles-js"> {/* Wrap everything in a parent div */}
             <header>
                 <h1>Michael Ikonomou</h1>
                 <nav>
